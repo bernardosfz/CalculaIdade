55e753317b7fc7521f103d541354eab66db981dd
bernardosfz <132398312+bernardosfz@users.noreply.github.com>
2023-09-12 13:23:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function calculaIdade(anoInformado) {
     anoAtual = new Date().getFullYear();
     let idade = anoAtual - anoInformado;
-    return idade
+    return idade;
 }
 
 anoInformado = prompt("Digite o ano que você nasceu: ");
-console.log(calculaIdade(anoInformado))
+console.log(calculaIdade(anoInformado));
