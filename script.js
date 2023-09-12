ac97c75ce12cb74ccc938b7e42c2f7328f703bdd
bernardosfz <132398312+bernardosfz@users.noreply.github.com>
2023-09-12 13:23:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculaIdade(anoInformado) {
+    anoAtual = new Date().getFullYear();
+    let idade = anoAtual - anoInformado;
+    return idade
+}
+
+anoInformado = prompt("Digite o ano que você nasceu: ");
+console.log(calculaIdade(anoInformado))
